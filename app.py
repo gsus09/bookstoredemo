684e91757307b6f12c5b21e2877ef12e88df29e9
Martin Thomas <martin.thomas@cariad.technology>
2025-11-03 10:25:06 +0100

    Refactor app.py to clean up imports and update request URL; update requirements.txt to specify package versions

--- app.py
+++ app.py
@@ -1,26 +1,20 @@
-import logging.config
-import os
-from flask import Flask, request, jsonify, render_template, redirect, url_for
+from flask import Flask, render_template
 import requests
-import json
-from flask import make_response
-from urllib.parse import urljoin
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    #put your live url here
-    request_url = "https://mytestapipython1235.azurewebsites.net/api/"
-    
+    request_url = "https://mytestapipython39.azurewebsites.net/api/"
     response = requests.get(request_url + 'getnotesmartin')
     posts = response.json()
-    print(posts)
     return render_template("index.html", posts=posts)
+
 
 def main():
     app.run(host='0.0.0.0', debug=True)
+
 
 if __name__ == '__main__':
     main()
